64693184586aa18d270b328932e97986b1f7820d
Markus Liechti <kaapiii@hotmail.com>
2017-06-29 15:41:21 +0200

    remove unused params, improve doc block

--- src/Kaapiii/Doctrine/BehavioralExtensions/Translatable/Transliterator.php
+++ src/Kaapiii/Doctrine/BehavioralExtensions/Translatable/Transliterator.php
@@ -2,23 +2,26 @@
 
 namespace Kaapiii\Doctrine\BehavioralExtensions\Translatable;
 
+use \Gedmo\Sluggable\Util\Urlizer;
+
 /**
  * Transliterator
  *
- * @author markus.liechti
+ * @author Markus Liechti <markus@liechti.io>
+ * @license MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
 class Transliterator {
-    
+
     /**
-     * Replace all German and French specail characters
+     * Replace all specail characters from German, French and some other languages
+     * Note: the default transliterator ignores most of the french special characters
      * 
      * @param string $slug
-     * @param string $separatorUsed
-     * @param string $objectBeingSlugged
+     * @param string $separator
      * 
      * @return string
      */
-    public function replaceSecialSigns($slug, $separatorUsed, $objectBeingSlugged){
+    public function replaceSecialSigns($slug, $separator = '-'){
         $specialCharactersPairs = array(
                 // German special characters
                 'ä'=>'ae', 'ö' => 'oe', 'ü' => 'ue', 'Ä' => 'Ae', 'Ö' => 'oe', 'Ü' => 'Üe', 'ß' => 'ss',
@@ -32,6 +35,6 @@ class Transliterator {
                 'ì'=>'i', 'í'=>'i', 'ð'=>'o', 'ñ'=>'n', 'ò'=>'o', 'ó'=>'o', 'ô'=>'o', 'õ'=>'o',
                 'ø'=>'o', 'ý'=>'y', 'ý'=>'y', 'þ'=>'b',
             );
-        return \Gedmo\Sluggable\Util\Urlizer::urlize(strtr($slug, $specialCharactersPairs));
+        return Urlizer::urlize(strtr($slug, $specialCharactersPairs), $separator);
     }
 }
